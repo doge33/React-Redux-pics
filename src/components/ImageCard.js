5f953e4439e7eb245e50e7c4d09d1a39034c777b
Sandy Long <sandylx33@gmail.com>
2021-01-14 22:05:03 -0500

    added Image card component and css to aset grid span on individual images

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,12 +2,44 @@ import React from 'react';
 
 //this component wraps each individual image (receives an image as props)
 export default class ImageCard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {spans: 0};
+    this.imageRef = React.createRef(); //create ref in constructor
+  }
+
+  // componentDidMount() {
+    
+  //   console.log(this.imageRef); // => you will see proper info including clientHeight
+  //   console.log(this.imageRef.current.clientHeight); // => you will see 0 because the image is not yet downloaded and loaded into the DOM at this point
+  // }
+
+  componentDidMount() {
+    
+    this.imageRef.current.addEventListener('load', this.setSpans); 
+    //any time an img successfully loads onto the screen, run setSpans, set the span for that card.
+  }
+
+
+  //span corresponds to grid-row-end unit
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10 );
+
+    //console.log(spans);
+
+    this.setState({spans: spans}) //or {spans} (ES2015)
+  }
+
   render() {
 
     const {description, urls} = this.props.image;
     return (
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
         <img 
+        ref={this.imageRef} //pass ref as props to JSX tag 
         alt={description}
         src={urls.regular}
         />
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,7 @@ import ImageCard from './ImageCard';
 import './ImageList.css';
 
 function ImageList(props) {
-                                  //  desstructre id url description from each image 
+                                  
   const images = props.images.map((image) => {
     return <ImageCard key={image.id} image={image} />
     // add key prop on the outer most element returned here
